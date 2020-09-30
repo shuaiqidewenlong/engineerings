64dbad439e3271b3f4ef94e2f46f0a3791fd3f2b
Administrator <2477692628@qq.com>
2020-09-30 11:01:07 +0800

    zwl0624

---
+++ src/main/java/com/itheima/pojo/LoginUsers.java
@@ -0,0 +1,15 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * @program: engineerings
+ * @author: wenlong
+ * @create: 2020-09-30 11:00
+ */
+@Data
+public class LoginUsers {
+    private  int uid;
+    private  String username;
+    private  String upassword;
+}
