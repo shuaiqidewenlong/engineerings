02f70389fb3d67f6a9eda67713d85fa0b72077d7
Administrator <2477692628@qq.com>
2020-09-30 10:33:09 +0800

    zwl0624

---
+++ src/main/java/com/itheima/engineering/EngineeringApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineeringApplication.class, args);
+        System.out.println("进去。。。。。。");
+    }
+
+}
