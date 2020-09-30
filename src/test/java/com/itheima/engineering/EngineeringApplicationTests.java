aecab9b9e109a084ad94df2ab54e69935462a687
Administrator <2477692628@qq.com>
2020-09-30 10:43:45 +0800

    zwl0624

---
+++ src/test/java/com/itheima/engineering/EngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
