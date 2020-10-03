b40d754b5d82d300939b1e5018cd60b926cb611c
Administrator <2477692628@qq.com>
2020-10-03 08:23:19 +0800

    zwl0624

--- src/main/java/com/itheima/controller/ZwlHtmlController.java
+++ src/main/java/com/itheima/controller/ZwlHtmlController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @create: 2020-10-02 13:29
  */
 @Controller
-public class ZwlHtmlConroller {
+public class ZwlHtmlController {
 
     @RequestMapping("login")
     String login(){
