5d00a978e960511804ff73555365a60498279573
fushaonan <1961223080@qq.com>
2020-10-03 09:07:05 +0800

    dd

--- src/main/java/com/itheima/controller/FsnController.java
+++ src/main/java/com/itheima/controller/FsnController.java
@@ -1,11 +1,13 @@
 package com.itheima.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class FsnController {
+    void  test(){
 
+        System.out.println("Test");
+    }
 }
